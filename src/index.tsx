bbfbfa5dc0e812dc1b6c1142c21d645d855ced50
Gyda Elisa Sæter <gyda@bekk-mac-3335.shared>
2024-09-16 13:50:20 +0200

    router fix

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {element: <App />,
+    path: "/",
+    loader: async () => {
+      return null;
+    }
+
+  }
+])
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
