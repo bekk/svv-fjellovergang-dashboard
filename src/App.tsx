f146b4a7b1cf633fa19ed9c97d7bd2c9b18ff53c
Gyda Elisa Sæter <gyda@bekk-mac-3335.shared>
2024-09-16 13:33:57 +0200

    csv read init

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { parse } from 'csv-parse/browser/esm/sync';
 import './App.css';
 
+type Predictions = {
+  date: string;
+  true_label: boolean;
+  prediction_confidence: number;
+  prediction: boolean;
+};
+
 function App() {
+  const [predictions, setPredictions] = useState<Predictions[]>([]);
+
+  useEffect(() => {
+    // Fetch the CSV file from the public folder
+    fetch('/data/lstm_predictions.csv')
+      .then(response => response.text())
+      .then(fileContent => {
+        const headers = ['date', 'true_label', 'prediction_confidence', 'prediction'];
+
+        // Parse the CSV content
+        const result: Predictions[] = parse(fileContent, {
+          delimiter: ',',
+          columns: headers,
+          skip_empty_lines: true
+        });
+
+        setPredictions(result);
+        console.log(predictions)
+      })
+      .catch(error => console.error('Error loading the CSV file:', error));
+  }, []);
+  
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <table>
+        <tr>
+          <th>date</th>
+          <th>true_label</th>
+          <th>prediction_confidence</th>
+          <th>prediction</th>
+        </tr>
+          { predictions.map(prediction => (
+          <tr>
+              <td>{prediction.date}</td>
+              <td>{prediction.true_label}</td>
+              <td>{prediction.prediction_confidence}</td>
+              <td>{prediction.prediction}</td>
+          </tr>  
+          ))} 
+      </table>
     </div>
   );
 }
